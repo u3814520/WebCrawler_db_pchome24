a5fb1165309691494e387f11856ee201d3113737
巧庭 <u3814520@gmail.com>
2021-10-24 11:15:39 +0800

    pchome24

---
+++ pchome24.py
@@ -0,0 +1,21 @@
+import requests
+import urllib.parse
+
+keywords=str(input("請輸入搜尋關鍵字:"))
+urlkeywords = urllib.parse.quote(keywords)
+
+
+r = requests.get(f'https://ecshweb.pchome.com.tw/search/v3.3/all/results?q={urlkeywords}&page={1}&sort=sale/dc')
+if r.status_code == requests.codes.ok:
+    data = r.json()
+    # pprint.pprint(data)
+    all_page = data['totalPage']
+    for page in range(1,all_page+1):
+        r = requests.get(f'https://ecshweb.pchome.com.tw/search/v3.3/all/results?q={urlkeywords}&page={page}&sort=sale/dc')
+        data = r.json()
+        for product in data['prods']:
+            name = product['name']
+            price = product['price']
+            print(name,price)
+
+
